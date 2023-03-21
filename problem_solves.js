46b4eaaf04b2e5037b9ecca5890329c6d60967b9
ibksdev <ibks.dev@gtmail.com>
2023-03-21 16:36:31 +0600

    4

--- problem_solves.js
+++ problem_solves.js
@@ -4,3 +4,20 @@ var i;
 for(i=0;i<=10;i++){
     console.log(i);
 }
+
+//challange #2: Print Odd Number less then 100
+
+var i;
+for(i=1;i<=100;i +=2)
+{
+    console.log(i);
+}
+
+for( var j=1; j<=10; j++)
+{
+    var row="7 x " + j + "=" + 7 * j;
+    
+    console.log(row)
+} 
+
+// Challange #3: 
